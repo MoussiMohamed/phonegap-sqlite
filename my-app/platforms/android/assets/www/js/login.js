36a88451e22bdeba6cd07f26fd1d5a9f046f86c4
John Doe <msi.moussi@gmail.com>
2015-08-12 14:34:16 +0100

    login from db device success

--- my-app/platforms/android/assets/www/js/login.js
+++ my-app/platforms/android/assets/www/js/login.js
@@ -3,8 +3,9 @@ var dbCreated = false;
  
 
 //document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady(){
- db = window.openDatabase("RegistrationDB", "1.0", "Registration", 200000);
+function doLogin(){
+	alert("entered in on device ready");
+ db = window.openDatabase("db_e_adv", "1.0", "db_e_adv", 2000000);
  if (dbCreated)
   {
       // created
@@ -17,7 +18,8 @@ function onDeviceReady(){
  
 function getregistdata(tx){
   
-  var sql = "select username, password from Registration";
+  var sql = "select u.email,u.password,at_r_u.id_role from user u, role r, attr_role_user at_r_u";
+
   tx.executeSql(sql, [], getlogin_success);
 }
  
@@ -28,14 +30,17 @@ function transaction_error(tx, error) {
 function getlogin_success(tx, results){
    var len = results.rows.length;
    for (var i=0; i< len; i++) {  
-    var employee = results.rows.item(i);
-    var username=document. getElementById("username").value;
-    var password=document. getElementById("psw").value;
-    var uname=employee.username;
-    var pasw=employee.password;
+    var user = results.rows.item(i);
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("psw").value;
+    var id_role_Delege=2;
+    var uname=user.email;
+    var pasw=user.password;
+   	var id_role=user.id_role;
     
-    if(username==uname && password==pasw){
+    if(username==uname && password==pasw && id_role==id_role_Delege){
      alert("Login Success");
+     window.open("nosProduits.html","_top");
      break;
     }
     else{
